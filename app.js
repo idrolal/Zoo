931973bdbbab5d2aadcfb1547c711f46486fcee2
Victor Semin <syominvictor@gmail.com>
2022-01-12 14:48:15 +0300

    hopePage

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const expressSession = require('express-session');
 const FileStore = require('session-file-store')(expressSession);
 const sessionMiddleware = require('./middleware/session');
 // const bcrypt = require('bcrypt');
+const indexRoute = require('./routes/index.route');
 
 const PORT = 4000;
 const app = express();
@@ -30,11 +31,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
 app.use(cookieParser());
 app.use(expressSession(sessionConfig));
 app.use(sessionMiddleware);
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRoute);
 
 app.listen(PORT, () => {
   console.log('The server has been started', PORT);
