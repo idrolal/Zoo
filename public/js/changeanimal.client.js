99596d80b882753ccc2f9ee6ed6d1fb419b03c23
alexrad555 <arayduyk@gmail.com>
2022-01-13 13:43:48 +0300

    add_button_del_animal

--- public/js/changeanimal.client.js
+++ public/js/changeanimal.client.js
@@ -9,3 +9,24 @@ changeanimal.addEventListener('submit', async (event) => {
   const json = await response.json();
   alert(json.message);
 });
+
+document.querySelectorAll('#buttonDelete')?.forEach((link) => link.addEventListener('click', async (event) => {
+  const animalId = event.target.value;
+
+  const body = {
+    animalId,
+  };
+
+  const buttonDevice = document.querySelector(`div[data-id="${animalId}"]`);
+  buttonDevice.remove();
+
+  const response = await fetch('/changeAnimal/delete', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const json = await response.json();
+  console.log(json.message);
+}));
