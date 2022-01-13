d36b13d8fcccad81272afa946b49b69dc3f6d994
Anna <anna.si4eva3000@gmail.com>
2022-01-13 13:57:28 +0300

    qwe

--- public/js/reg.js
+++ public/js/reg.js
@@ -1,25 +1,25 @@
-const reg = document.getElementById('reg');
+// const reg = document.getElementById('reg');
 
-reg.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const { method, action } = event.target;
-  const body = {
-    email: event.target.name.email,
-    password: event.target.name.password,
-  };
+// reg.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+//   const { method, action } = event.target;
+//   const body = {
+//     email: event.target.name.email,
+//     password: event.target.name.password,
+//   };
 
-  const response = await fetch(action, {
-    method,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  });
-  const json = await response.json();
+//   const response = await fetch(action, {
+//     method,
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(body),
+//   });
+//   const json = await response.json();
 
-  if (json.isAdmin) {
-    window.location.href = '/';
-  } else {
-    alert('Что-то пошло не так');
-  }
-});
+//   if (json.isAdmin) {
+//     window.location.href = '/';
+//   } else {
+//     alert('Что-то пошло не так');
+//   }
+// });
--- routes/reg.route.js
+++ routes/reg.route.js
@@ -1,25 +1,25 @@
-const route = require('express').Router();
-const bcrypt = require('bcrypt');
-const { Admin } = require('../db/models');
+// const route = require('express').Router();
+// const bcrypt = require('bcrypt');
+// const { Admin } = require('../db/models');
 
-route.get('/', (req, res) => {
-  res.render('reg');
-});
+// route.get('/', (req, res) => {
+//   res.render('reg');
+// });
 
-route.post('/', async (req, res) => {
-  const { email, password } = req.body;
+// route.post('/', async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const admin = await Admin.create({
-      email,
-      password: await bcrypt.hash(password, 10),
-    });
-    req.session.admin = admin;
-    req.session.isAdmin = true;
-    res.json({ isAdmin: true });
-  } catch (error) {
-    res.json({ error: error.message });
-  }
-});
+//   try {
+//     const admin = await Admin.create({
+//       email,
+//       password: await bcrypt.hash(password, 10),
+//     });
+//     req.session.admin = admin;
+//     req.session.isAdmin = true;
+//     res.json({ isAdmin: true });
+//   } catch (error) {
+//     res.json({ error: error.message });
+//   }
+// });
 
-module.exports = route;
+// module.exports = route;
