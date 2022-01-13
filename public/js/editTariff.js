22288fb909189e4151efe9929abecbbf77d5a2ae
Anna <anna.si4eva3000@gmail.com>
2022-01-13 17:55:14 +0300

    tar

--- public/js/editTariff.js
+++ public/js/editTariff.js
@@ -1,4 +1,23 @@
-document.querySelectorAll('#editBtn')?.forEach((btn) => btn.addEventListener('click', (event) => {
+const editTariff = document.getElementById('editTariff');
+
+editTariff.addEventListener('submit', async (event) => {
   event.preventDefault();
-  console.log('123');
-}));
+  const body = {
+    name: event.target.name.value,
+    description: event.target.description.value,
+    price: event.target.price.value,
+    id: event.target.id.value,
+  };
+
+  const response = await fetch('/editTariff', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const json = await response.json();
+  if (json.message === 'wow') {
+    window.location.href = '/editTariff';
+  }
+});
--- routes/editTariff.route.js
+++ routes/editTariff.route.js
@@ -1,5 +1,38 @@
 const route = require('express').Router();
+const pool = require('pg').pool();
 const { Tariff } = require('../db/models');
 
+route.get('/', async (req, res) => {
+  const tariff = await Tariff.findAll();
+  res.render('tariffPage', {
+    tariff,
+    isAdmin: req.session.isAdmin,
+    name: req.session.admin?.name,
+    password: req.session.admin?.password,
+  });
+});
+
+route.get('/:id', async (req, res) => {
+  const { id } = req.body;
+  pool.query('SELECT * FROM tariff WHERE id=?', [id], (err, data) => {
+    if (err) return alert('ooops');
+    return res.render('tariffPage', {
+      tariff: data[0],
+    });
+  });
+});
+
+route.post('/:id', async (req, res) => {
+  const { name, description, price } = req.body;
+  //   const tariff = await Tariff.update({
+  //     name,
+  //     description,
+  //     price,
+  //   });
+  pool.query('UPDATE tariff SET name=?, description=?, price=? WHERE id=?', [name, description, price], (err, data) => {
+    if (err) return alert('opps');
+    return res.json({ message: 'wow' });
+  });
+});
 
 module.exports = route;
